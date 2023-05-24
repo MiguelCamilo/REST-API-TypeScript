bb4e90fe9afca7942f125eaeec794bc67a1950d1
Miguel Camilo <miguelcamilo48@gmail.com>
2023-05-24 19:20:34 -0400

    ADDED sessionToken to user registration

--- src/controllers/register.ts
+++ src/controllers/register.ts
@@ -16,7 +16,7 @@ export const register = async (req: express.Request, res: express.Response) => {
         const existingUser = await getUserByEmail(email);
         // returns error if true
         if (existingUser) {
-            return res.status(400).send({ message: 'User already exists.' });
+            return res.status(403).send({ message: 'Email is already taken.' });
         }
         
         const salt = random() // creates random string
@@ -28,6 +28,9 @@ export const register = async (req: express.Request, res: express.Response) => {
                 password: authentication(salt, password) // passing back the two params we set up in the helper function
             }
         })
+
+        user.authentication.sessionToken = authentication(salt, user._id.toString())
+        res.cookie('AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' })
         // returns the new user in a json format
         return res.status(200).json(user).end();
 
